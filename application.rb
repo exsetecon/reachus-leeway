7721eaead553835f263d7f86cedcaf6081bf7aab
Exsete <connect@exsete.com>
2015-06-29 18:17:52 +0530

    testing 1.4

--- application.rb
+++ application.rb
@@ -74,7 +74,7 @@ post '/send_resume' do
             :content => params[:message]
             }]
     message = {"attachments"=>
-            [{"type"=>params[:mime],
+            [{"type"=>params[:Mime],
                 "name"=>"resume.docx",
                 "content"=>params[:fileData]],
             "to"=>
